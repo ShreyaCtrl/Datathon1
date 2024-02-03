84a5dba3cb10bc5c9dbe89e3150c5461a0f42192
Siddhi Shah <shahsiddhi777@gmail.com>
2024-02-04 02:31:45 +0530

    chat bot frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,57 @@ import './App.css';
 import HomePage from "./components/homePage";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import AboutPage from "./components/AboutPage";
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import bot from './assets/img/bot.png';
 
 function App() {
+  const chatBotSteps = [
+    {
+        id: '0',
+        message: 'Hey Geek!',
+        trigger: '1',
+    }, {
+        id: '1',
+ 
+    
+        message: 'Please write your username',
+        trigger: '2'
+    }, {
+        id: '2',
+        user: true,
+        trigger: '3',
+    }, {
+        id: '3',
+        message: " hi {previousValue}, how can I help you?",
+        trigger: 4
+    }, {
+        id: '4',
+        options: [
+            { value: 1, label: 'View Courses' },
+            { value: 2, label: 'Read Articles' },
+ 
+        ],
+        end: true
+    }
+];
+ 
+const chatBotTheme = {
+    background: '#C9FF8F',
+    headerBgColor: '#197B22',
+    headerFontSize: '20px',
+    botBubbleColor: '#0F3789',
+    headerFontColor: 'white',
+    botFontColor: 'white',
+    userBubbleColor: '#FF5733',
+    userFontColor: 'white',
+};
+ 
+
+const chatBotConfig = {
+    botAvatar: bot,
+    floating: true,
+};
   return (
     <div className="App">
      <BrowserRouter>
@@ -22,6 +71,13 @@ function App() {
        <Route path='/about' element={<AboutPage/>}/>
       </Routes>
       {/* <Footer/> */}
+      <ThemeProvider theme={chatBotTheme}>
+          <ChatBot
+            headerTitle="ResearchBot"
+            steps={chatBotSteps}
+            {...chatBotConfig}
+          />
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
